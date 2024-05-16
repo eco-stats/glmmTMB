885a5904f99ac40af5a6df34b0ddca342dd67104
Elliot Dovers <elliot.dovers@gmail.com>
2024-05-16 14:17:02 +1000

    minor update to biplot aesthetics

--- glmmTMB/R/mvlgcp.R
+++ glmmTMB/R/mvlgcp.R
@@ -1106,15 +1106,18 @@ get_bfs_coeff <- function(object) {
 #'
 #' @param x a fitted model object from \code{mvlgcp}.
 #' @param ... additional parameters as in \code{stats::biplot}
-#' @param alpha a numeric in [0,1] indicating the scaling of arrows/lines representing factor loading lengths. Defaults to 0.5
+#' @param alpha a numeric in [0,1] indicating the scaling of arrows/lines representing factor loading lengths. Defaults to the ratio of the maximum L2 norms for the scores to factor loadings.
 #' @param load.names (optional) a character vector of length \code{object$n_reponses} of labels to be used on response-specific loadings. Default is \code{NULL} which means the fitted model's \code{response.id} will be used to determine labels.
 #' @param score.col (optional) a vector of length \code{object$n_basis} of colours to be used for scores.
 #' @param load.col (optional) a vector of length \code{object$n_reponses} of colours to be used on response-specific loadings.
 #' @param load.name.cex (optional) a numeric indicating the scaling of response label size.
-#' @param show.top.n (optional) a numeric in [0,\code{object$n_reponses}] indicating a subset of the response-specific loadings (from largest to smallest).
-#' @param vmax.rotation a logical indicating whether to perform a varimax rotation of the loadings matrix prior to plotting.
-#' @param rm.bias a logical indicating whether to remove bias from both the scores and factor loadings by scaling them # TODO: checks required
+#' @param show.top.n (optional) a numeric in [1,\code{object$n_reponses}] indicating the number of response-specific loadings to show (from largest to smallest).
+#' @param show.responses (optional) a numeric vector with elements that are response indices (\emph{i.e.} in [1,\code{object$n_reponses}]) indicating a subset of the response-specific loadings to be plotted.
+#' @param load.lab.offset a numeric, indicating the offset for response labels on the factor loadings. Defaults to 0.1
+#' @param do.svd a logical indicating whether to additionally perform a Singular Value Decomposition on the approximate basis function coefficient matrix. Default is \code{FALSE}. Primarily used to include the additional independent fields so that the loadings are transformed to represent correlation between point patterns.
+#' @param incl.ind.flds a logical indicating whether to include basis function coefficients from independent fields (if the model has these) within the Singular Value Decomposition - will only apply when \code{do.svd = TRUE}, in which case the default is \code{TRUE}.
 #' @param show.all.arrows a logical indicating whether to plot all arrows/lines, even when \code{show.top.n} is used.
+#' @param rotate.by.theta a numeric, indicating a rotation of the biplot (in Radians).
 #' 
 #' @return See \code{stats::biplot}
 #' @exportS3Method stats::biplot glmmTMB
@@ -1130,66 +1133,101 @@ get_bfs_coeff <- function(object) {
 #' m <- mvlgcp(pt ~ (1 | tree), data = dat, weights = dat$wt, basis.functions = bfs, response.id = dat$tree)
 #' 
 #' biplot(m)
-biplot.glmmTMB <- function(x, ..., alpha = 0.5, load.names, score.col, load.col, load.name.cex = 1, show.top.n, vmax.rotation = FALSE, rm.bias = T, show.all.arrows = F) {
+biplot.glmmTMB <- function(x, ..., alpha, load.names, score.col, load.col, load.name.cex = 1, show.top.n, show.responses, load.lab.offset, do.svd = FALSE, incl.ind.flds = TRUE, show.all.arrows = FALSE, rotate.by.theta) {
 
   # extract the factor loadings and basis function coefficients
   fact_loads <- get_loadings(x)
   bf.coeffs <- get_bfs_coeff(x)
   
-  if (vmax.rotation) {
-    rot <- varimax(fact_loads)
-    Lambda <- rot$loadings
-    b <- bf.coeffs %*% rot$rotmat
-  } else {
-    if (rm.bias) {
-      Lambda <- scale(fact_loads)
-      b <- scale(bf.coeffs)
+  # checks of the correct factor-analytic structure
+  if (sum(grepl("d", colnames(bf.coeffs), fixed = T)) < 2) {
+    if (sum(grepl("d", colnames(bf.coeffs), fixed = T)) == 0){
+      stop("No correlated latent fields found. Biplotting requires an mvlgcp with 2 latent factors.")
     } else {
-      Lambda <- fact_loads
-      b <- bf.coeffs
+      stop("Only a single correlated latent field found within the mvlgcp - Biplotting requires 2.")
     }
   }
-  # if (ncol(Lambda) > 2) {
-  #   warning("More than two latent factors found. Only the first two will be used")
-  # }
   if (sum(grepl("d", colnames(bf.coeffs), fixed = T)) > 2) {
     warning("More than two latent factors found. Only the first two will be used")
   }
+
+  # check if we want to additionally perform a Singular Value Decomp.
+  if (do.svd) {
+    A <- (bf.coeffs[ , c("d1", "d2")] %*% t(fact_loads))
+    # check if we want to add in the coefficients from the independent fields
+    if (incl.ind.flds) {
+      A <- A + bf.coeffs[ , grepl("m", colnames(bf.coeffs), fixed = T)]
+    }
+    A_svd <- svd(A)
+    scores <- A_svd$u[ , 1:2]
+    loads <- A_svd$v[ , 1:2] %*% t(diag(A_svd$d[1:2]))
+  } else {
+    scores <- bf.coeffs[ , c("d1", "d2")]
+    loads <- fact_loads
+  }
   
-  # order the loadings by arrow lengths
-  arrow_lengths <- apply(Lambda, 1, function(x){sqrt(x[1]^2 + x[2]^2)})
-  floads <- Lambda[order(arrow_lengths, decreasing = T), ]
-  # get arrow names
+  # check and rotate the biplot if necessary
+  if (!missing(rotate.by.theta)) {
+    R <- matrix(c(cos(rotate.by.theta), -sin(rotate.by.theta), sin(rotate.by.theta), cos(rotate.by.theta)), nrow = 2)
+    loads <- loads %*% t(R)
+    scores <- scores %*% t(R)
+  }
+  
+  # get response names
   if (missing(load.names)) {
     resp.names <- unique(x$response.id)
     load.names <- if (is(resp.names, "factor")) {levels(resp.names)} else {as.character(unique(resp.names))}
   }
-  # subset the factor loading if required
-  if (missing(show.top.n)) {
-    show.top.n <- nrow(fact_loads)
+  
+  # subset the loadings if specified
+  if (!missing(show.responses)) {
+    if (!all(show.responses %in% 1:length(load.names))) {
+      stop("response indices provided in 'show.responses' are not all within 1:n_responses")
+    }
+    loads <- loads[show.responses, ]
+    load.names <- load.names[show.responses]
   }
-  sub.floads <- floads[1:show.top.n, ]
+
+  # order the loadings by arrow lengths
+  arrow_lengths <- apply(loads, 1, norm, type = "2")
+  loads <- loads[order(arrow_lengths, decreasing = T), ]
+
+  # further subset the factor loadings to the largest n if required
+  if (missing(show.top.n)) {
+    show.top.n <- nrow(loads)
+  }
+  sub.loads <- loads[1:show.top.n, ]
   sub.names <- load.names[order(arrow_lengths, decreasing = T)][1:show.top.n]
   # determine colors
   if (missing(score.col)) {
     score.col <- "black"
-  } 
+    score.pch <- 1
+  } else {
+    score.pch <- 16
+  }
   if (missing(load.col)) {
     load.col <- "black"
   } else {
     load.col <- load.col[order(arrow_lengths, decreasing = T)][1:show.top.n]
   }
+  # determine the alpha scaling (for loadings)
+  if (missing(alpha)) {
+    alpha <- max(apply(scores, 1, norm, type = "2")) / max(apply(sub.loads, 1, norm, type = "2"))
+  }
+  # determine the label offset for loadings
+  if (missing(load.lab.offset)) {
+    load.lab.offset <- mean(apply(scores, 1, norm, type = "2")) / 10
+  }
   
   ### Create the biplot ########################################################
   {
-    # par(mar = c(3.1,2.1,0.5,0),mgp=c(1.75,0.75,0))
-    plot(b[ , 1:2], xlim = range(b[,1]), ylim = range(b[,2]),
-         pch = 16, col = score.col, ...)
-    text(x = (sub.floads[,1] * alpha) + 0.1 * sign(sub.floads[,1]), y = (sub.floads[,2] * alpha) + 0.1 * sign(sub.floads[,2]), labels = sub.names, cex = load.name.cex, offset = 0.8, col=load.col)
+    plot(scores, xlim = range(c(scores, sub.loads * alpha)), ylim = range(c(scores, sub.loads * alpha)),
+         pch = score.pch, col = score.col, ...)
+    text(x = (sub.loads[,1] * alpha) + (load.lab.offset * sign(sub.loads[,1])), y = (sub.loads[,2] * alpha) + (load.lab.offset * sign(sub.loads[,2])), labels = sub.names, cex = load.name.cex, col=load.col)#, offset = 0.8)
     if (show.all.arrows) {
-      arrows(x0 = rep(0, nrow(floads)), y0 = rep(0, nrow(floads)), x1 = floads[,1] * alpha, y1 = floads[,2] * alpha, length = 0.0, angle = 30, lwd = 0.5, col=load.col)
+      arrows(x0 = rep(0, nrow(loads)), y0 = rep(0, nrow(loads)), x1 = loads[,1] * alpha, y1 = loads[,2] * alpha, length = 0.0, angle = 30, lwd = 0.5, col=load.col)
     } else {
-      arrows(x0 = rep(0, nrow(sub.floads)), y0 = rep(0, nrow(sub.floads)), x1 = sub.floads[,1] * alpha, y1 = sub.floads[,2] * alpha, length = 0.0, angle = 30, lwd = 0.5, col=load.col)
+      arrows(x0 = rep(0, nrow(sub.loads)), y0 = rep(0, nrow(sub.loads)), x1 = sub.loads[,1] * alpha, y1 = sub.loads[,2] * alpha, length = 0.0, angle = 30, lwd = 0.5, col=load.col)
     }
   }
 }
